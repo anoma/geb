94d64e6433d00bc3b01cfc819d20a9480c5a21d3
Terence Rokop <rokopt@sharpears.net>
2022-11-15 15:28:11 -0800

    Add a skeleton lambda-conversion test module

---
+++ geb-cl/test/lambda-conversion.lisp
@@ -0,0 +1,6 @@
+(in-package :geb-test)
+
+(def-suite geb.lambda-conversion
+    :description "Tests the geb.lambda-conversion package")
+
+(in-suite geb.lambda-conversion)
--- geb-cl/test/run-tests.lisp
+++ geb-cl/test/run-tests.lisp
@@ -1,7 +1,7 @@
 (in-package :geb-test)
 
 (defparameter *all-tests*
-  (list 'geb 'geb.lambda))
+  (list 'geb 'geb.lambda 'geb.lambda-conversion))
 
 (defun run-tests ()
   (mapc #'run! *all-tests*))
