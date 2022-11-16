162358eae0874d12274fcbf8c1ab2cb384745d77
mariari <mariari@protonmail.com>
2022-11-16 16:20:22 +0800

    Move the test of compile-unit to lambda-conversions

--- geb-cl/test/lambda-conversion.lisp
+++ geb-cl/test/lambda-conversion.lisp
@@ -4,3 +4,16 @@
     :description "Tests the geb.lambda-conversion package")
 
 (in-suite geb.lambda-conversion)
+
+(def so-unit-type
+  geb:so1)
+
+(def stlc-unit-term
+  geb.lambda.spec:unit)
+
+(def so-unit-term
+  (geb:terminal so-unit-type))
+
+(test compile-unit
+  (is (obj-equalp (conversion:compile-checked-term nil so-unit-type stlc-unit-term)
+                  so-unit-term)))
--- geb-cl/test/lambda.lisp
+++ geb-cl/test/lambda.lisp
@@ -35,17 +35,3 @@
 (test compile-nil-context
   (is (equalp (geb.lambda-conversion:stlc-ctx-to-mu nil)
               geb:so1)))
-
-(def so-unit-type
-  geb:so1)
-
-(def stlc-unit-term
-  geb.lambda.spec:unit)
-
-(def so-unit-term
-  (geb:terminal so-unit-type))
-
-(test compile-unit
-  (is (obj-equalp (geb.lambda-conversion:compile-checked-term
-                   nil so-unit-type stlc-unit-term)
-                  so-unit-term)))
