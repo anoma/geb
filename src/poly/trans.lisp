d0fb0954c45deaf716016a4cd32a5ed1ff64de4c
mariari <mariari@protonmail.com>
2022-11-22 15:45:05 +0800

    Add a few more cases

--- src/poly/trans.lisp
+++ src/poly/trans.lisp
@@ -25,13 +25,38 @@
 (defmethod morph-to-poly ((obj geb:inject-right))
   (+ (obj-to-nat (mcar obj)) ident))
 
-(defmethod morph-to-poly ((obj geb:case))
-  (+ (obj-to-nat (mcar obj)) ident))
-
 (defmethod morph-to-poly ((obj geb:comp))
   (compose (morph-to-poly (mcar obj))
            (morph-to-poly (mcadr obj))))
 
+(defmethod morph-to-poly ((obj geb:case))
+  obj
+  (error "not implemented"))
+
+(defmethod morph-to-poly ((obj geb:project-right))
+  (let ((nat (obj-to-nat (mcadr obj))))
+    (if (zerop nat)
+        nat
+        (mod ident nat))))
+
+(defmethod morph-to-poly ((obj geb:project-left))
+  (let ((nat (obj-to-nat (mcar obj))))
+    (if (zerop nat)
+        nat
+        (/ ident nat))))
+
+(defmethod morph-to-poly ((obj geb:distribute))
+  (let ((cx (obj-to-nat (mcar obj)))
+        (cy (obj-to-nat (mcadr obj)))
+        (cz (obj-to-nat (mcaddr obj))))
+    (if (and (zerop cy) (zerop cz))
+        0
+        (let* ((yz   (+ cy cz))
+               (xin  (/ ident yz))
+               (yzin (mod ident yz)))
+          (if-lt yzin cy
+                 (+ (* cy xin) yzin)
+                 (+ (* cz xin) (- yzin cy) (* cx cy)))))))
 
 (defun obj-to-nat (obj)
   obj
