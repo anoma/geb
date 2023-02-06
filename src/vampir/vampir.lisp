2dd328d77bc7accebe822fce2469655b0cdbebed
Terence Rokop <rokopt@sharpears.net>
2023-02-06 10:54:21 -0800

    Define next-range and its application procedure
    Similar to the boolean commit, but for a range procedure that lets us
    extract n bits of a term

--- src/vampir/vampir.lisp
+++ src/vampir/vampir.lisp
@@ -145,3 +145,52 @@ of ()'s for any non normal form"
 
 (defun bool (x)
   (make-application :func :bool :arguments (list x)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Range inductive definition up to 32-bits
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defparameter *next-range*
+  (let ((a-wire (make-wire :var :a)))
+    (make-alias
+     :name :next_range
+     :inputs (list :range :a)
+     :body
+     (list
+      (make-bind
+       :names (list (make-wire :var :a0))
+       :value (make-application
+               :func :bool
+               :arguments (list
+                           (make-application
+                            :func :fresh
+                            :arguments
+                            (list
+                             (make-infix :op :%
+                                         :lhs a-wire
+                                         :rhs (make-constant :const 2)))))))
+      (make-bind :names (list (make-wire :var :a1))
+                 :value
+                 (make-application :func :fresh
+                                   :arguments
+                                   (list
+                                    (make-infix :op :\\
+                                                :lhs a-wire
+                                                :rhs (make-constant :const 2)))))
+      (make-equality :lhs a-wire
+                     :rhs
+                     (make-infix :op :+
+                                 :lhs (make-wire :var :a0)
+                                 :rhs (make-infix :op :*
+                                                  :lhs (make-constant :const 2)
+                                                  :rhs (make-wire :var :a1))))
+      (make-tuples :wires (list
+                           (make-wire :var :a0)
+                           (make-application :func :range
+                                             :arguments
+                                             (list (make-wire :var :a1)))))))))
+
+(defun next-range (range x)
+  (make-application :func :next_range
+                    :arguments (list range x)))
