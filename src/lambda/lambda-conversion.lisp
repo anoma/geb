7827f950337b477f946a5c9d9a021669e2db27d2
Terence Rokop <rokopt@sharpears.net>
2023-02-06 09:53:14 -0800

    Use object-specific equality test for objects
    This is intended to fix issue #47.

--- src/lambda/lambda-conversion.lisp
+++ src/lambda/lambda-conversion.lisp
@@ -34,11 +34,11 @@
        (pair (compile-checked-term context lty l)
              (compile-checked-term context rty r)))
       ((fst lty rty value)
-       (assert (eql (class-of lty) (class-of type)) nil "Types should match on fst: ~A ~A"
+       (assert (geb.mixins:obj-equalp (class-of lty) (class-of type)) nil "Types should match on fst: ~A ~A"
                term type)
        (comp (<-left lty rty) (compile-checked-term context (prod lty rty) value)))
       ((snd lty rty value)
-       (assert (eql (class-of rty) (class-of type)) nil "Types should match on fst: ~A ~A"
+       (assert (geb.mixins:obj-equalp (class-of rty) (class-of type)) nil "Types should match on fst: ~A ~A"
                term type)
        (comp (<-right lty rty) (compile-checked-term context (prod lty rty) value)))
       ((lamb vty tty term)
@@ -46,7 +46,7 @@
                ;; is this correct?
                (compile-checked-term (cons vty context) tty term))))
       ((app dom com f x)
-       (assert (eql dom type) nil "Types should match for application: ~A ~A" dom type)
+       (assert (geb.mixins:obj-equalp dom type) nil "Types should match for application: ~A ~A" dom type)
        (comp
         (so-eval dom com)
         (pair (compile-checked-term context dom f)
