f99bfdcefc8233709d96c62e050f0ac78b2bf2e1
mariari <mariari@protonmail.com>
2023-02-07 12:23:54 +0800

    Create a test to make sure #47 succeeds
    This tests the pipeline still wroks even with the old entry code gone

--- test/pipeline.lisp
+++ test/pipeline.lisp
@@ -3,12 +3,23 @@
 (define-test geb-pipeline :parent geb-test-suite)
 
 (def test-compilation-eval-2
-  (geb.lambda.spec:typed geb.lambda.spec:unit geb:so1))
+  (lambda:typed geb.lambda.spec:unit geb:so1))
 
+
+(defparameter *entry*
+  (lambda:typed
+   (lambda:app (coprod so1 so1)
+               (coprod so1 so1)
+               (lambda:lamb (coprod so1 so1) (coprod so1 so1) (lambda:index 0))
+               (lambda:left lambda:unit))
+   (coprod so1 so1)))
 
 (define-test pipeline-works-for-stlc-to-vampir
   :parent geb-pipeline
   (parachute:finish
    (geb.entry:compile-down :vampir t
                            :stlc t
-                           :entry "geb-test::test-compilation-eval-2")))
+                           :entry "geb-test::test-compilation-eval-2"))
+  (parachute:finish
+   (geb.entry:compile-down :stlc t
+                           :entry "geb-test::*entry*")))
