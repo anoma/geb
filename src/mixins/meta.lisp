d7c194fe6d262012c471cce07acdae80b5186d72
mariari <mariari@protonmail.com>
2023-02-14 14:49:00 +0800

    Add weak pointer functionality
    This is to prevent storing objects in the value slot that would
    otherwise be free go unchecked.

--- src/mixins/meta.lisp
+++ src/mixins/meta.lisp
@@ -9,24 +9,28 @@
 with the given object. @MIXIN-PERFORMANCE covers my performance
 characteristics"))
 
-(-> meta-insert (meta-mixin t t) t)
-(defun meta-insert (object key value)
+(-> meta-insert (meta-mixin t t &key (:weak t)) t)
+(defun meta-insert (object key value &key weak)
   "Inserts a value into storage. If the key is a one time object, then
 the insertion is considered to be volatile, which can be reclaimed
 when no more references to the data exists.
 
 If the data is however a constant like a string, then the insertion is
-considered to be long lived and will always be accessible"
+considered to be long lived and will always be accessible
+
+The :weak keyword specifies if the pointer stored in the value is weak"
   (let ((hash (or (gethash object (meta object))
                   (setf (gethash object (meta object))
                         (tg:make-weak-hash-table :test 'equalp
                                                  :weakness :key)))))
     (setf (gethash key hash)
-          value)))
+          (if weak (tg:make-weak-pointer value) value))))
 
 (-> meta-lookup (meta-mixin t) t)
 (defun meta-lookup (object key)
-  "Lookups the requested key in the metadata table of the object"
+  "Lookups the requested key in the metadata table of the object. We
+look past weak pointers if they exist"
   (let ((table (gethash object (meta object))))
     (when table
-      (gethash key table))))
+      (let ((value (gethash key table)))
+        (if (tg:weak-pointer-p value) (tg:weak-pointer-value value) value)))))
