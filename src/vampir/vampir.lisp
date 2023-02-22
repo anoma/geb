cd102fdba1a1998898a04892a0ca4a99593bd429
Artem Gureev <artemgureev@yahoo.com>
2023-02-22 22:42:00 +0600

    Fix if-then parameters

--- src/vampir/vampir.lisp
+++ src/vampir/vampir.lisp
@@ -264,10 +264,10 @@
                                               :lhs (make-infix :op :-
                                                                :lhs (make-constant :const 1)
                                                                :rhs t-wire)
-                                              :rhs p-wire)
+                                              :rhs q-wire)
                              :rhs (make-infix :op :*
                                               :lhs t-wire
-                                              :rhs q-wire))))))
+                                              :rhs p-wire))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Pointwise modulus
