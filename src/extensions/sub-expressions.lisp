6e4f3b883c2c042260a9d74f22ac3650e03b4a47
mariari <mariari@protonmail.com>
2023-04-20 17:14:13 +0800

    Modify the recursive functions to also work on lists

--- src/extensions/sub-expressions.lisp
+++ src/extensions/sub-expressions.lisp
@@ -2,31 +2,41 @@
 
 (deftype obj-morph () `(or cat-obj cat-morph t))
 
+(-> can-continuep (t) boolean)
+(defun can-continuep (term)
+  (and (or (typep term 'pointwise-mixin)
+           (typep term 'list))
+       (not (typep term 'cat-obj))
+       (not (typep term 'number))
+       (not (typep term 'string))
+       (not (typep term 'geb.vampir.spec:constant))))
+
 (-> common-sub-expressions (obj-morph) (values obj-morph fset:map))
 (defun common-sub-expressions (term)
   "Compute common sub-expressions and return an object with the
 appropriate sub-expressions uniquely identified"
-  (keep-unique term (compute-common-usages term)))
+  (if (can-continuep term)
+      (keep-unique term (compute-common-usages term))
+      (values term (fset:empty-map))))
 
 (-> compute-common-usages (obj-morph) fset:bag)
 (defun compute-common-usages (obj)
   ;; we are going to be smart about this
   ;; no manual recursion, we don't need that
-  (labels ((recursive (bag term)
-             (reduce-pointwise (lambda (bag term)
-                                 ;; we don't care about reducing out cat-objs
-                                 (cond ((or (not (typep term 'pointwise-mixin))
-                                            (typep term 'cat-obj))
-                                        bag)
-                                       ;; we don't want to actually add
-                                       ;; to a total that is already
-                                       ;; count
-                                       ((fset:member? term bag)
-                                        (fset:with bag term))
-                                       (t
-                                        (recursive (fset:with bag term) term))))
-                               term
-                               bag)))
+  (labels ((reduce-fn (bag term)
+             (cond ((not (can-continuep term))
+                    bag)
+                   ;; we don't want to actually add
+                   ;; to a total that is already
+                   ;; count
+                   ((fset:member? term bag)
+                    (fset:with bag term))
+                   (t
+                    (recursive (fset:with bag term) term))))
+           (recursive (bag term)
+             (if (listp term)
+                 (reduce #'reduce-fn term :initial-value bag)
+                 (reduce-pointwise #'reduce-fn term bag))))
     (values
      (fset:filter-pairs (lambda (x y) (declare (ignore x)) (<= 2 y))
                         (recursive (fset:empty-bag) obj)))))
@@ -45,11 +55,13 @@ This is part two of the COMMON-SUB-EXPRESSIONS pass."
                                (values x (gensym)))
                              (fset:convert 'fset:map bag))))
     (labels ((recursive (obj)
-               (if (or (not (typep obj 'pointwise-mixin))
-                       (typep obj 'cat-obj))
+               (if (not (can-continuep obj))
                    (values obj mapping)
                    (let ((looked  (fset:lookup mapping obj))
-                         (new-obj (map-pointwise #'recursive obj)))
+                         (new-obj
+                           (if (listp obj)
+                               (mapcar #'recursive obj)
+                               (map-pointwise #'recursive obj))))
                      (if looked
                          (make-common-sub-expression :obj new-obj :name looked)
                          new-obj)))))
