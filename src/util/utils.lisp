b0409492271638039869c0acd2988439482a08ed
Artem Gureev <artemgureev@yahoo.com>
2023-05-01 20:51:10 +0600

    Apply-n Utility Added
    Adds a utility function applying a given function
    n-times recursively. Useful for iterative calls
    of Geb functions.

--- src/util/utils.lisp
+++ src/util/utils.lisp
@@ -201,3 +201,19 @@ Further this can be used in type signatures
   (:documentation
    "the then branch of the
 [object](http://www.lispworks.com/documentation/HyperSpec/Body/26_glo_o.htm#object)"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;               Additional Utils
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defun apply-n (times f initial)
+  "Applies a function, f, n TIMES to the INITIAL values
+
+```lisp
+GEB> (apply-n 10 #'1+ 0)
+10 (4 bits, #xA, #o12, #b1010)
+```"
+  (let ((value initial))
+    (dotimes (n times value)
+      (setf value (funcall f value)))))
