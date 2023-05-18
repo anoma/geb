daeb0a6eb434cf89bca038557a45d71e6d26e386
mariari <mariari@protonmail.com>
2023-05-18 08:34:59 +0800

    Add a evaluation tests for the maybe data type
    We semantically check if the operations we know behave properly

--- test/geb-trans.lisp
+++ test/geb-trans.lisp
@@ -18,3 +18,49 @@
   (is obj-equalp #*1000 (gapply geb.pair-of-size-4 #*0)
       "Right should tag it with 1, with our 0 injected and our left should
        always be 00"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                    Interpreter tests                      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-test geb.trans-eval :parent geb.trans)
+
+(def bitc-yes
+  (gapply (to-bitc dec:inj-yes) #*))
+
+(def bitc-no
+  (gapply (to-bitc dec:inj-no) #*))
+
+(def bitc-maybe
+  (gapply (to-bitc dec:inj-maybe) #*))
+
+(defun bitc-pair (&rest morphisms)
+  "pairs the constructors and then calls bitc. only with constructors"
+  (gapply (to-bitc (apply #'pair morphisms)) #*))
+
+(define-test geb.trans-eval-maybe :parent geb.trans-eval
+  ;; merge tests
+  (is equalp
+      bitc-yes
+      (gapply (to-bitc dec:merge-opinion)
+              (bitc-pair dec:inj-maybe dec:inj-yes)))
+  (is equalp
+      bitc-maybe
+      (gapply (to-bitc dec:merge-opinion)
+              (bitc-pair dec:inj-no dec:inj-yes)))
+  (is equalp
+      bitc-no
+      (gapply (to-bitc dec:merge-opinion)
+              (bitc-pair dec:inj-no dec:inj-maybe)))
+  (is equalp
+      bitc-no
+      (gapply (to-bitc dec:merge-opinion)
+              (bitc-pair dec:inj-maybe dec:inj-no)))
+
+  ;; promote and demote tests
+  (is equalp bitc-no    (gapply (to-bitc dec:demote) bitc-no))
+  (is equalp bitc-no    (gapply (to-bitc dec:demote) bitc-maybe))
+  (is equalp bitc-maybe (gapply (to-bitc dec:demote) bitc-yes))
+  (is equalp bitc-yes   (gapply (to-bitc dec:promote) bitc-yes))
+  (is equalp bitc-yes   (gapply (to-bitc dec:promote) bitc-maybe))
+  (is equalp bitc-maybe (gapply (to-bitc dec:promote) bitc-no)))
