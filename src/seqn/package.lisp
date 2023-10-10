8d8cf6cd1c30d57a51b277fba9a232b9d55ef578
Artem Gureev <artemgureev@yahoo.com>
2023-10-10 16:40:47 +0600

    Remove Cod and Dom Generics
    Removes unnecessary cod/dom generics for SeqN.

--- src/seqn/package.lisp
+++ src/seqn/package.lisp
@@ -4,7 +4,8 @@
  (uiop:define-package #:geb.seqn.main
    (:mix #:geb.mixins #:geb.generics #:geb.spec
          #:geb.extension.spec #:geb.seqn.spec
-         #:serapeum #:common-lisp)))
+         #:serapeum #:common-lisp)
+   (:export :cod :dom #:@seqn-api)))
 
 (in-package :geb.seqn.main)
 
@@ -19,9 +20,7 @@
   (inj-coprod-parallel pax:function)
   (zero-list              pax:function)
   (dom                 (pax:method () (<seqn>)))
-  (dom                 (pax:generic-function))
   (cod                 (pax:method () (<seqn>)))
-  (cod                 (pax:generic-function))
   (well-defp-cat       (pax:method () (<seqn>)))
   (gapply              (pax:method () (<seqn> t))))
 
