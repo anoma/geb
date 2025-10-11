8878b737fb18737e55b292d6f2b52a6252e02e09
Terence M. Rokop <rokopt@sharpears.net>
2025-10-11 07:34:00 -0700

    Add a couple imports

--- geb-lean/GebLean.lean
+++ geb-lean/GebLean.lean
@@ -1,4 +1,6 @@
 import GebLean.Utilities
+import GebLean.Semicategory
+import GebLean.FiniteQuiver
 import GebLean.CategoryJudgments
 import GebLean.DepCategoryJudgments
 import GebLean.AcyclicQuiver
