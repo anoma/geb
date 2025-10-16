4f30f21584d079182990ec0cb293d7bbcb2155e3
Terence M. Rokop <rokopt@sharpears.net>
2025-10-16 12:19:56 -0700

    Tiny refactoring in `FintypeData`

--- geb-lean/GebLean/Utilities/Fintype.lean
+++ geb-lean/GebLean/Utilities/Fintype.lean
@@ -20,13 +20,16 @@ universe u
 
 variable {α : Type u}
 
+/-- Property that a finset contains all elements of a type. -/
+abbrev FinsetComplete (s : Finset α) : Prop := ∀ x : α, x ∈ s
+
 /-- Structure-based representation of a finite type: a finset containing
     all elements. -/
 structure FintypeData (α : Type u) where
   /-- The finite set of all elements -/
   elems : Finset α
   /-- Property that the finset contains all elements -/
-  complete : ∀ x : α, x ∈ elems
+  complete : FinsetComplete elems
 
 /-- Build a `Fintype` typeclass instance from `FintypeData`. -/
 instance (data : FintypeData α) : Fintype α where
