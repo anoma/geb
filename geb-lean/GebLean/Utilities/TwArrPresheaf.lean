993eefc28fdb79af1e6363c5253c26479844ef36
Terence M. Rokop <rokopt@sharpears.net>
2025-11-25 17:02:28 -0800

    Make use of sliceEquivCopresheaf

--- geb-lean/GebLean/Utilities/TwArrPresheaf.lean
+++ geb-lean/GebLean/Utilities/TwArrPresheaf.lean
@@ -1,15 +1,29 @@
+import GebLean.Utilities.Elements
 import GebLean.Utilities.TwistedArrow
 
 /-!
-# Presheaf and copresheaf categories on twisted arrow categories
+# Functor categories on twisted arrow categories
 
 This module defines functor categories from the four twisted arrow category
-variants to `Type`:
+variants to `Type v`:
 
-- `TwArrCopresheaf C` = `TwistedArrow' C ⥤ Type` (covariant functors)
-- `TwArrPresheaf C` = `OpTwistedArrow' C ⥤ Type` (contravariant functors)
-- `TwArrOpCopresheaf C` = `TwistedArrowOp' C ⥤ Type` (covariant functors)
-- `TwArrOpPresheaf C` = `CoTwistedArrow C ⥤ Type` (contravariant functors)
+- `TwArrCopresheaf C` = `TwistedArrow' C ⥤ Type v` (copresheaves on Tw)
+- `TwArrPresheaf C` = `OpTwistedArrow' C ⥤ Type v` (presheaves on Tw)
+- `TwArrOpCopresheaf C` = `TwistedArrowOp' C ⥤ Type v` (copresheaves on TwOp)
+- `TwArrOpPresheaf C` = `CoTwistedArrow C ⥤ Type v` (presheaves on TwOp)
+
+Since `OpTwistedArrow' C ≅ (TwistedArrow' C)ᵒᵖ'`, functors from `OpTwistedArrow'`
+are presheaves on `TwistedArrow'`. Similarly, since
+`CoTwistedArrow C ≅ (TwistedArrowOp' C)ᵒᵖ'`, functors from `CoTwistedArrow`
+are presheaves on `TwistedArrowOp'`.
+
+Two of these have direct slice equivalences via `sliceEquivCopresheaf`:
+- `Over hom' ≌ TwArrCopresheaf C`
+- `Over homOp' ≌ TwArrOpCopresheaf C`
+
+The other two (`TwArrPresheaf` and `TwArrOpPresheaf`) use `ElementsContra'`
+rather than `Elements`, so expressing them as `Over` categories requires
+additional work.
 -/
 
 universe v u
@@ -22,39 +36,59 @@ variable (C : Type u) [Category.{v} C]
 
 /--
 Copresheaves on the twisted arrow category: covariant functors from
-`TwistedArrow' C` to `Type`.
+`TwistedArrow' C` to `Type v`.
 -/
-def TwArrCopresheaf := TwistedArrow' C ⥤ Type (max u v)
+def TwArrCopresheaf := TwistedArrow' C ⥤ Type v
 
 instance : Category (TwArrCopresheaf C) := by
   unfold TwArrCopresheaf
   infer_instance
 
 /--
-Presheaves on the twisted arrow category: contravariant functors from
-`TwistedArrow' C` to `Type`, i.e., covariant functors from `OpTwistedArrow' C`.
+The slice category over `hom'` is equivalent to the category of copresheaves
+on the twisted arrow category.
 -/
-def TwArrPresheaf := OpTwistedArrow' C ⥤ Type (max u v)
+def sliceEquivTwArrCopresheaf : Over (hom' (C := C)) ≌ TwArrCopresheaf C :=
+  sliceEquivCopresheaf (hom' (C := C))
+
+/--
+Presheaves on the twisted arrow category: covariant functors from
+`OpTwistedArrow' C` to `Type v`.
+
+Since `OpTwistedArrow' C ≅ (TwistedArrow' C)ᵒᵖ'`, these are contravariant
+functors on `TwistedArrow' C`, i.e., presheaves.
+-/
+def TwArrPresheaf := OpTwistedArrow' C ⥤ Type v
 
 instance : Category (TwArrPresheaf C) := by
   unfold TwArrPresheaf
   infer_instance
 
 /--
-Copresheaves on the opposite twisted arrow category: covariant functors from
-`TwistedArrowOp' C` to `Type`.
+Copresheaves on the opposite-variant twisted arrow category: covariant functors
+from `TwistedArrowOp' C` to `Type v`.
 -/
-def TwArrOpCopresheaf := TwistedArrowOp' C ⥤ Type (max u v)
+def TwArrOpCopresheaf := TwistedArrowOp' C ⥤ Type v
 
 instance : Category (TwArrOpCopresheaf C) := by
   unfold TwArrOpCopresheaf
   infer_instance
 
 /--
-Presheaves on the opposite twisted arrow category: contravariant functors from
-`TwistedArrowOp' C` to `Type`, i.e., covariant functors from `CoTwistedArrow C`.
+The slice category over `homOp'` is equivalent to the category of copresheaves
+on the opposite-variant twisted arrow category.
 -/
-def TwArrOpPresheaf := CoTwistedArrow C ⥤ Type (max u v)
+def sliceEquivTwArrOpCopresheaf : Over (homOp' (C := C)) ≌ TwArrOpCopresheaf C :=
+  sliceEquivCopresheaf (homOp' (C := C))
+
+/--
+Presheaves on the opposite-variant twisted arrow category: covariant functors
+from `CoTwistedArrow C` to `Type v`.
+
+Since `CoTwistedArrow C ≅ (TwistedArrowOp' C)ᵒᵖ'`, these are contravariant
+functors on `TwistedArrowOp' C`, i.e., presheaves.
+-/
+def TwArrOpPresheaf := CoTwistedArrow C ⥤ Type v
 
 instance : Category (TwArrOpPresheaf C) := by
   unfold TwArrOpPresheaf
