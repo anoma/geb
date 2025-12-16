b6eed0822a08136e22cf2f7b1dc67e74e6d44fe7
Terence M. Rokop <rokopt@sharpears.net>
2025-12-16 09:51:25 -0800

    Some more flexible category-judgment universes

--- geb-lean/GebLean/PLang/CatJudgment.lean
+++ geb-lean/GebLean/PLang/CatJudgment.lean
@@ -1,3 +1,6 @@
+import Mathlib.CategoryTheory.Category.Basic
+import Mathlib.CategoryTheory.Types.Basic
+
 /-!
 # Categorical Judgments for PLang
 
@@ -7,6 +10,33 @@ This module defines the copresheaf of judgments about categories.
 namespace GebLean
 
 namespace PLang
+
+open CategoryTheory
+
+def ObjCopr.{u} : Type u := Sort u
+
+def ObjMorObj.{u, v} : Type (max u v) := ObjCopr.{u} × Sort v
+
+def ObjMorProj.{u, v} (om : ObjMorObj.{u, v}) : Sort (imax v u) := om.2 → om.1
+
+def ObjMorMor.{u, v} (om : ObjMorObj.{u + 1, v + 1}) : Type (max u v) :=
+  ObjMorProj.{u + 1, v + 1} om × ObjMorProj.{u + 1,v + 1} om
+
+def ObjMorCopr.{u, v} : Type (max u v + 1) :=
+  Σ (o : ObjMorObj.{u + 1, v + 1}), ObjMorMor.{u, v} o
+
+def IdProj.{u, v, w} (om : ObjMorObj.{u, v}) (i : Sort w) : Sort (imax w v) :=
+  i → om.2
+
+def ObjMorIdObj.{u, v, w} : Type (max u v w) :=
+  ObjMorObj.{u, v} × Sort w
+
+def ObjMorIdMor.{u, v, w} (omi : ObjMorIdObj.{u + 1, v + 1, w + 1}) :
+  Type (max u v w) :=
+    ObjMorMor.{u, v} omi.1 × IdProj.{u + 1, v + 1, w + 1} omi.1 omi.2
+
+def ObjMorIdCopr.{u, v, w} : Type (max u v w + 1) :=
+  Σ (omi : ObjMorIdObj.{u + 1, v + 1, w + 1}), ObjMorIdMor.{u, v, w} omi
 
 end PLang
 
