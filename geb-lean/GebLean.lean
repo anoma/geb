02923674071d59896a2cdaebf50b73e369f53a5c
Terence M. Rokop <rokopt@sharpears.net>
2026-04-14 08:26:38 -0700

    Prove every ER term's interpretation is Nat.Primrec'

--- geb-lean/GebLean.lean
+++ geb-lean/GebLean.lean
@@ -41,6 +41,7 @@ import GebLean.LawvereBTPrimrec
 import GebLean.LawvereBTQuot
 import GebLean.LawvereER
 import GebLean.LawvereERArith
+import GebLean.LawvereERPrimrec
 import GebLean.LawvereERBool
 import GebLean.LawvereERDelta
 import GebLean.LawvereERInterp
