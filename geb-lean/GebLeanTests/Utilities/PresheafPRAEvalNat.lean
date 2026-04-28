40479f373f335b1b757ac9eca490191c0e0982e4
Terence M. Rokop <rokopt@sharpears.net>
2026-04-28 11:09:03 -0700

    tests: add bridge collapse tests for praPolyEvalLaxNatTrans

--- geb-lean/GebLeanTests/Utilities/PresheafPRAEvalNat.lean
+++ geb-lean/GebLeanTests/Utilities/PresheafPRAEvalNat.lean
@@ -36,4 +36,28 @@ example (G : Cat.{0, 0}ᵒᵖ ⥤ Cat.{0, 0}) :
     LaxNatTransContraData G G :=
   LaxNatTransContraData.ofNatTrans (𝟙 G)
 
+/-! ## Bridge collapse -/
+
+example (I : Cat.{0, 0}) :
+    praPolyEvalLaxNatTrans.{0, 0, 0, 0, 0, 0}.app I =
+    praPolyEvalAtIFunctor.{0, 0, 0, 0, 0, 0} I :=
+  praPolyEvalAtIFunctor_eq_praPolyEvalLaxNatTrans_app I
+
+section WeakBridge
+variable (I : Cat.{0, 0}) (J : Cat.{0, 0})
+  (P : PresheafPRACat.{0, 0, 0, 0, 0, 0} I J)
+  (Z : ↑(presheafCat.{0, 0, 0} I))
+
+example : ((praEvalAtFunctor.{0, 0, 0, 0, 0, 0} I J).obj P).obj Z =
+    (ULift.down.{1, 1}
+      (ULiftHom.objDown.{1, 1}
+        (GrothendieckContraFunctor.objFiber
+          ((praPolyEvalLaxNatTrans.{0, 0, 0, 0, 0, 0}.app I).obj
+            (praPolyEvalAtISourceObj.{0, 0, 0, 0, 0, 0}
+              I J P Z)))) :
+      Jᵒᵖ ⥤ Type 0) :=
+  praEvalAtFunctor_via_praPolyEvalLaxNatTrans I J P Z
+
+end WeakBridge
+
 end GebLeanTests.Utilities.PresheafPRAEvalNat
